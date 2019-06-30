489f80131e136b901d5b2eb0815021a38ee87bd9
Oper80 <oper_80@mail.ru>
2019-06-30 09:40:02 +0200

    Init commit for hometask_2

---
+++ app/src/main/java/ru/skillbranch/devintensive/User.kt
@@ -0,0 +1,40 @@
+package ru.skillbranch.devintensive
+
+import java.util.*
+
+class User (
+    val id:String,
+    var firstName:String?,
+    var lastName:String?,
+    var avatar:String?,
+    var rating:Int = 0,
+    var respect:Int = 0,
+    val lastVisit: Date? = null,
+    val isOnline:Boolean = false
+) {
+    constructor(id: String, firstName: String?, lastName: String?) : this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    constructor(id: String) : this(id, "John", "Doe $id")
+
+    init {
+        println("It's Alive!!!")
+    }
+
+    fun printMe():Unit{
+        println("""
+            id: $id
+            firstName: $firstName
+            lastName: $lastName
+            avatar: $avatar
+            rating: $rating
+            respect: $respect
+            lastVisit: $lastVisit
+            isOnline: $isOnline
+        """.trimIndent())
+    }
+}
